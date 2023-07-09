55a2ab4c54a648223d93a73299c486a9e136dbb6
gholami71 <careless.whisper51@yahoo.com>
2023-07-09 12:42:17 +0430

    //

--- src/component/infoUser.js
+++ src/component/infoUser.js
@@ -1,8 +1,28 @@
 
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { CiUser } from "react-icons/ci";
+import { OnRun } from "../config/OnRun";
+import { getCookie } from "../function/cookie";
 
 
 const InfoUser = () =>{
+
+    const [userCredit, setuserCredit] = useState({'datecredit':'', 'lable':''})
+
+    const cookie = getCookie('phu')
+
+
+    const UserInfo  = () =>{
+        axios.post(OnRun+'/user/userinfo', {cookie:cookie})
+        .then(Response=>{
+            console.log(Response.data)
+        })
+
+    }
+
+    useEffect(UserInfo, [])
+
     return(
         <div className="InfoUser">
             <div className="Info">
