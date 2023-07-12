5a13207c852df97ccbabcfc08bf189363cf557e2
gholami71 <careless.whisper51@yahoo.com>
2023-07-12 15:58:55 +0430

    //

--- src/component/pricing.js
+++ src/component/pricing.js
@@ -2,12 +2,19 @@ import { useEffect, useState } from "react"
 
 
 const Pricing = () =>{
-    const [data, setData] = useState({time:1, label:''})
+    const [data, setData] = useState({time:1})
     const [price, setPrice] = useState({pro:1000, proPlus:1500,primium:2000})
 
 
     const handlePrice = () =>{
-        setPrice({pro:1000*time, proPlus:1000*time,primium:1000*time})
+        if (data.time != 1) {
+            setPrice({pro:(1000*data.time)*(1-(data.time/24)), proPlus:(1500*data.time)*(1-(data.time/24)),primium:(2000*data.time)*(1-(data.time/24))})            
+        }
+        else{
+            setPrice({pro:1000*data.time, proPlus:1500*data.time,primium:2000*data.time})            
+
+        }
+        console.log(data)
     }
 
     useEffect(handlePrice,[data.time])
@@ -15,13 +22,13 @@ const Pricing = () =>{
     return(
         <div className="container-page">
             <div className="TimePricing">
-                <input type="radio" checked={data.time == 1} onChange={(e)=>setData({...data,time:e.target.value})} value='OneMonth' id="OneMonth"></input>
+                <input type="radio" checked={data.time == 1} onChange={(e)=>setData({...data,time:e.target.value})} value='1' id="OneMonth"></input>
                 <label htmlFor="OneMonth">یک ماهه</label>
-                <input type="radio" checked={data.time == 3} onChange={(e)=>setData({...data,time:e.target.value})} value='TreeMonth' id="TreeMonth"></input>
+                <input type="radio" checked={data.time == 3} onChange={(e)=>setData({...data,time:e.target.value})} value='3' id="TreeMonth"></input>
                 <label htmlFor="TreeMonth">سه ماهه</label>
-                <input type="radio" checked={data.time == 6} onChange={(e)=>setData({...data,time:e.target.value})} value='SixMonth' id="SixMonth"></input>
+                <input type="radio" checked={data.time == 6} onChange={(e)=>setData({...data,time:e.target.value})} value='6' id="SixMonth"></input>
                 <label htmlFor="SixMonth">شش ماهه</label>
-                <input type="radio" checked={data.time == 12} onChange={(e)=>setData({...data,time:e.target.value})} value='OnYear' id="OnYear"></input>
+                <input type="radio" checked={data.time == 12} onChange={(e)=>setData({...data,time:e.target.value})} value='12' id="OnYear"></input>
                 <label htmlFor="OnYear">یک ساله</label>
             </div>
             <div className="TimeDetail">
