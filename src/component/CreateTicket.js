cc4fd68e6e546d8f4d926a18539f51e882e3a651
gholami71 <careless.whisper51@yahoo.com>
2023-07-24 16:13:52 +0430

    //

--- src/component/CreateTicket.js
+++ src/component/CreateTicket.js
@@ -16,7 +16,14 @@ const CreateTicket = (props) =>{
 
 
     const handlerSetTicket = () =>{
-        axios.post(OnRun+'/user/setticket',{phu:phu,ticket:ticket})
+        if(ticket.title == ''){
+            toast.warning('لطفا عنوان را وارد کنید',{position: toast.POSITION.BOTTOM_RIGHT,className: 'negetive-toast'});
+        }
+        else if(ticket.content == ''){
+            toast.warning('لطفا توضیحات را به درستی وارد کنید.',{position: toast.POSITION.BOTTOM_RIGHT,className: 'negetive-toast'});
+
+        }else{
+            axios.post(OnRun+'/user/setticket',{phu:phu,ticket:ticket})
         .then(response=>{
             if (response.data.reply) {
                 toast.success('تیکت ثبت شد',{position: toast.POSITION.BOTTOM_RIGHT,className: 'negetive-toast'});
@@ -29,6 +36,8 @@ const CreateTicket = (props) =>{
         .catch(err=>{
             toast.error(err,{position: toast.POSITION.BOTTOM_RIGHT,className: 'err-toast'});
         })
+        }
+        
     }
 
 
@@ -37,14 +46,19 @@ const CreateTicket = (props) =>{
         <ToastContainer autoClose={3000} />
         {
             props.popUp?
+                <>
                 <div className="PopUp">
-                    <span className="closeIcon" onClick={(e)=>{props.setPopUp(!props.popUp)}}><RxCross1/></span>
+                    <span className="closeIcon" onClick={()=>{props.setPopUp(!props.popUp)}}><RxCross1/></span>
                     <input className="TicketTitle" value={ticket.title} onChange={(e)=>setTicket({...ticket,title:e.target.value})} placeholder="عنوان"></input>
                     <textarea value={ticket.content} onChange={(e)=>setTicket({...ticket,content:e.target.value})} placeholder="درخواست"></textarea>
                     <button onClick={handlerSetTicket}>ثبت</button>
                 </div>
+                <div className="hide" onClick={()=>props.setPopUp(false)}></div>
+                </>
+
                 :null
         }
+
         </>
     )
 
